1fbf84c1fa18ffbcd1b8943606bd9d2106d6c1c2
Aquino <jorgeacruz@google.com>
2025-09-11 00:55:54 +0000

    w

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__)
 
- Define a route to serve the index.html file
+# Define a route to serve the index.html file
 @app.route('/')
 def index():
     return render_template('index.html')
 
- Define a route for page_two
+# Define a route for page_two
 @app.route('/page_two')
 def page_two():
     return render_template('page_two.html')
